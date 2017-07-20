46d0ca29c2c102f3932639dcb55d3ac8be94ad16
jzd <jzd.tsai@gmail.com>
2017-07-20 20:02:17 +0800

    OK

--- resources/views/backend/products/index.blade.php
+++ resources/views/backend/products/index.blade.php
@@ -8,14 +8,20 @@
             @if($products != '')
                 @foreach($products as $list)
                 <div class="panel panel-default">
+                    <div class="panel-heading">
+                        商品名稱->{{ $list->title}}
+                    </div>
+
+                    <form action="#" method="post">
+                        {{ csrf_field() }}
+                        <input type="hidden" value="{{$list->id}}" name="product_id">
+                        <input type="submit" class="btn btn-danger" value="修改" name="product_id">
+                    </form>
                     {!! Form::open(array('action' => array('backend\ProductsController@destroy', $request->store_id , $list->id), 'class' => 'form-inline', 'method' => 'delete')) !!}
 
                     {{ Form::button('刪除', ['type' => 'submit', 'class' => 'btn btn-danger',  'title' => '刪除'] )  }}
 
                     {!! Form::close() !!}
-                    <div class="panel-heading">
-                        商品名稱->{{ $list->title}}
-                    </div>
 
                     <div class="panel-body">
                         <img src="{{ $list->cover }}" width="150px" height="150px">
