507fe27bd8b7bd93f5bf585fa971da29c3c32529
jzd <jzd.tsai@gmail.com>
2017-07-20 12:50:27 +0800

    333

--- resources/views/backend/products/index.blade.php
+++ resources/views/backend/products/index.blade.php
@@ -18,7 +18,7 @@
                         <input type="hidden" value="{{$list->id}}" name="product_id">
                         <input type="submit" value="修改" name="product_id">
                     </form>
-                    <form action="/stores/{{$list->store_id}}/products/{{$list->id}}" method="DELETE">
+                    <form action="/stores/{{$list->store_id}}/products" method="DELETE">
                         <input type="hidden" value="{{$list->id}}" name="product_id">
                         <input type="submit" value="刪除" name="product_id">
                     </form>
