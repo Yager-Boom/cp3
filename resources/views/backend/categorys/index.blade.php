51b4fd44c94edd25ccc7a2ccf5080b6c7521a374
jzd <jzd.tsai@gmail.com>
2017-07-21 15:45:46 +0800

    123

--- resources/views/backend/categorys/index.blade.php
+++ resources/views/backend/categorys/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-;ekrgargbh'[aqreghbn'[aroi;
+    <a href="#" class="btn btn-info pull-right">店家頁</a>
+    <a href="/backend/category" class="btn btn-info pull-right">目錄頁</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="/backend/stores/create">Create</a>
+                1234567890
+            </div>
+        </div>
+    </div>
 @endsection
