0a3cf998bc4adc092e12b69f58f321c2a6b96b14
jzd <jzd.tsai@gmail.com>
2017-07-21 16:51:28 +0800

    asd

--- resources/views/backend/stores/create.blade.php
+++ resources/views/backend/stores/create.blade.php
@@ -4,9 +4,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
+            @foreach(123 as )
             {!! Form::open(array('action' => array('backend\StoresController@store'), 'files'=>true)) !!}
             @include('backend/stores/_form')
             {!! Form::close() !!}
+            @endforeach
         </div>
     </div>
 </div>
