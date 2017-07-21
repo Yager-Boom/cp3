e57c0010fd2e1842ab9730ac5e1a27e2fe4baacc
jzd <jzd.tsai@gmail.com>
2017-07-21 15:51:51 +0800

    345

--- resources/views/backend/categorys/index.blade.php
+++ resources/views/backend/categorys/index.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="#" class="btn btn-info pull-right">店家頁</a>
-    <a href="/backend/category" class="btn btn-info pull-right">目錄頁</a>
+    <a href="/backend" class="btn btn-info pull-right">店家頁</a>
+    <a href="#" class="btn btn-info pull-right">目錄頁</a>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <a href="/backend/stores/create">Create</a>
-                1234567890
+                <div class="panel-heading">
+                    目錄名稱
+                </div>
+
+                <div class="panel-body">
+                    名稱名稱名稱...
+                </div>
             </div>
         </div>
     </div>
