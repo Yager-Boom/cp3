9e497ea3c4d5eb26ab9b13eab7bef902d2c800e2
jzd <jzd.tsai@gmail.com>
2017-07-21 16:31:41 +0800

    asd

---
+++ resources/views/backend/categorys/_form.blade.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="form-group">
+	        {{ Form::text('domain', null , ['class' => 'form-control', 'placeholder' => '輸入商品分類']) }}
+        </div>
+            {{ Form::submit('建立', array('class' => 'btn btn-success pull-right')) }}
+        <a href="/backend">上一頁</a>
+    </div>
+</div>
