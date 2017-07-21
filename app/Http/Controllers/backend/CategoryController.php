90bd9164c563cb04b7d95fe5c433e80f6ec15343
jzd <jzd.tsai@gmail.com>
2017-07-21 15:32:45 +0800

    55688

---
+++ app/Http/Controllers/backend/CategoryController.php
@@ -0,0 +1,15 @@
+<?php namespace App\Http\Controllers\backend;
+
+use Session;
+use Input;
+use Illuminate\Http\Request;
+use App\Http\Controllers\BackendController as Controller;
+use App\Services\ImgService;
+
+
+class CategoryController extends Controller
+{
+    function index(){
+        dd(55688);
+    }
+}
