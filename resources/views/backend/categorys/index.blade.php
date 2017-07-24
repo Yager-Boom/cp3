89e1d7de03e6e7dc62822ea0310c3988445948bf
jzd <jzd.tsai@gmail.com>
2017-07-24 15:19:47 +0800

    form edit & delete

--- resources/views/backend/categorys/index.blade.php
+++ resources/views/backend/categorys/index.blade.php
@@ -5,15 +5,19 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <a href="/backend/stores/{{$detail->id}}/category/create">Create</a>
                 @foreach($details as $detail)
                     <div style="border-style:solid;">
                         商店網址{{$detail->domain}}
                         <div class="panel-heading">
-                            <a href="/backend/stores/{{$detail->id}}/category/create">Create</a>
-                            <br>
-                            <a href="/backend/stores/{{$detail->id}}/category/edit">Edit</a>
-                            <br>
-                            <a href="/backend/stores/{{$detail->id}}/category/delete">Delete</a>
+                                <br>
+                            {!! Form::open(array('action' => array('backend\CategoryController@edit', $request->store_id , $list->id), 'class' => 'form-inline', 'method' => 'edit')) !!}
+                                {{ Form::button('修改', ['type' => 'submit', 'class' => 'btn btn-danger',  'title' => '修改'] )  }}
+                            {!! Form::close() !!}
+                                <br>
+                            {!! Form::open(array('action' => array('backend\CategoryController@destroy', $request->store_id , $list->id), 'class' => 'form-inline', 'method' => 'delete')) !!}
+                                {{ Form::button('刪除', ['type' => 'submit', 'class' => 'btn btn-danger',  'title' => '刪除'] )  }}
+                            {!! Form::close() !!}
                         </div>
                         <div class="panel-body">
                             <ul>
