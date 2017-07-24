94ce6e50f34419aa88fc96d059a544f377349e45
jzd <jzd.tsai@gmail.com>
2017-07-24 12:15:04 +0800

    123

--- resources/views/backend/categorys/create.blade.php
+++ resources/views/backend/categorys/create.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            {{--{!! Form::open(array('action' => array('backend\CategoryController@store'), 'files'=>true)) !!}--}}
-            {{--@include('backend/categorys/_form')--}}
-            {{--{!! Form::close() !!}--}}
+            {!! Form::open(array('action' => array('backend\CategoryController@store'), 'files'=>true)) !!}
+            @include('backend/categorys/_form')
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
