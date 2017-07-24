e9ae29c8832c537604ae1212832ebf178765f610
jzd <jzd.tsai@gmail.com>
2017-07-24 14:41:25 +0800

    details 06

--- resources/views/backend/categorys/index.blade.php
+++ resources/views/backend/categorys/index.blade.php
@@ -5,8 +5,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                @foreach($stores as $store)
-                    <a href="/backend/stores/{{$store->id}}/category/create">Create...商店網址{{$store->domain}}</a>
+                @foreach($details as $detail)
+                    <a href="/backend/stores/{{$store->id}}/category/create">Create...商店網址{{$detail->domain}}</a>
                     <div class="panel-heading">
                         目錄名稱
                     </div>
