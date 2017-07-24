a26b8e1e8ef9e29f25967a78014b012daa3da001
jzd <jzd.tsai@gmail.com>
2017-07-24 12:47:24 +0800

    666666

--- app/Http/Controllers/backend/CategoryController.php
+++ app/Http/Controllers/backend/CategoryController.php
@@ -4,11 +4,19 @@ use Session;
 use Input;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BackendController as Controller;
+use App\Services\StoreService;
 use App\Services\ImgService;
 
 
 class CategoryController extends Controller
 {
+    public function __construct(StoreService $storeService, ImgService $imgService)
+    {
+        $this->middleware('isStore');
+        $this->storeService = $storeService;
+        $this->imgService = $imgService;
+    }
+
     function index()
     {
         $stores = $this->storeService->getLists($this->User()->id);
