1678e080567beb86b3b23f0a19a1e2f58bcb0eeb
jzd <jzd.tsai@gmail.com>
2017-07-24 13:26:23 +0800

    create 01

--- resources/views/backend/categorys/_form.blade.php
+++ resources/views/backend/categorys/_form.blade.php
@@ -3,6 +3,23 @@
         <div class="form-group">
             {{ Form::text('category', null , ['class' => 'form-control', 'placeholder' => '輸入商品分類']) }}
         </div>
+
+        <div class="form-group">
+            {{ Form::text('store_id', null , ['class' => 'form-control', 'placeholder' => 'store_id']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::text('link', null , ['class' => 'form-control', 'placeholder' => 'link']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::text('position', null , ['class' => 'form-control', 'placeholder' => 'position']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::text('nitem', null , ['class' => 'form-control', 'placeholder' => 'nitem']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::text('sort', null , ['class' => 'form-control', 'placeholder' => 'sort']) }}
+        </div>
+
             {{ Form::submit('建立', array('class' => 'btn btn-success pull-right')) }}
         <a href="/backend">上一頁</a>
     </div>
