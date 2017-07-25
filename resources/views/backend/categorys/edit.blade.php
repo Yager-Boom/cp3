3cb5c047fba492b6a79b5f5123ae855e659125c8
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-07-25 16:32:03 +0800

    asd

---
+++ resources/views/backend/categorys/edit.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            {{--{!! Form::open(array('action' => array('backend\CategoryController@update',$stores->id))) !!}--}}
+                {{--123--}}
+            {{--{!! Form::close() !!}--}}
+        </div>
+    </div>
+</div>
+@endsection
