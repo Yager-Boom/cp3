dcca91d2dcfa8dc272762e779291ebcef4e0f9fb
jzd <jzd.tsai@gmail.com>
2017-07-25 13:28:22 +0800

    destroy

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -11,8 +11,19 @@ class CategoryService
     {
         $this->categoryRepository = $categoryRepository;
     }
-    public function store()
+    public function details($uid)
     {
-
+        return  \DB::table('user_stores')
+                   ->select('*','navs.id as nid', 'user_stores.store_id as usid')
+                   ->leftjoin('navs','navs.store_id', 'user_stores.store_id')
+                   ->leftjoin('stores','stores.id', 'navs.store_id')
+                   ->where('user_stores.user_id',$uid)
+                   ->get();
+    }
+    public function destroy($nid)
+    {
+        \DB::table('navs')
+            ->where('id',$nid)
+            ->delete();
     }
 }
