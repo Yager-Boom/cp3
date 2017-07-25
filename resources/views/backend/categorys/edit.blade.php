90f7acb04db953f860f4488339582b0f464f5fa1
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-07-25 16:45:54 +0800

    asd

--- resources/views/backend/categorys/edit.blade.php
+++ resources/views/backend/categorys/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            {!! Form::open(array('action' => array('backend\CategoryController@update',$edit_navs), 'method' => 'post')) !!}
+            {!! Form::open(array('action' => array('backend\CategoryController@update',$stores->id,$edit_navs), 'method' => 'get')) !!}
                 123
             {!! Form::close() !!}
         </div>
