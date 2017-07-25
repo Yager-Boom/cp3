19ef016713bd884c42b6ab714fb6dff98a2c5068
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-07-25 19:23:50 +0800

    asd

--- resources/views/backend/categorys/edit.blade.php
+++ resources/views/backend/categorys/edit.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            {{--{!! Form::open(array('action' => array('backend\CategoryController@update'),$stores->id,$edit_navs, 'class' => 'form-inline', 'method' => 'get')) !!}--}}
-                {{--@include('backend/categorys/_form')--}}
-            {{--{!! Form::close() !!}--}}
+            {!! Form::open(array('action' => array('backend\CategoryController@update'),$stores->id,$edit_navs, 'class' => 'form-inline', 'method' => 'get')) !!}
+                @include('backend/categorys/_form')
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
