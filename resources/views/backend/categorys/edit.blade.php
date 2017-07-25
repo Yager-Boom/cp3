3332fc652e05a9105816b30007042f9c53cd6e3d
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-07-25 16:38:31 +0800

    asd

--- resources/views/backend/categorys/edit.blade.php
+++ resources/views/backend/categorys/edit.blade.php
@@ -4,9 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            {{--{!! Form::open(array('action' => array('backend\CategoryController@update',$stores->id))) !!}--}}
-                {{--123--}}
-            {{--{!! Form::close() !!}--}}
+            {!! Form::open(array('action' => array('backend\CategoryController@update',$edit_navs->id))) !!}
+                123
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
