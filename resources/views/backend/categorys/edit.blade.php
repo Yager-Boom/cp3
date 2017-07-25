8c5b28d1d8ba6009d6715c29cc1b0f5ba65ea8da
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-07-25 19:57:54 +0800

    asd

--- resources/views/backend/categorys/edit.blade.php
+++ resources/views/backend/categorys/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             {!! Form::open(array('action' => array('backend\CategoryController@update', $store_id, $category), 'class' => 'form-inline', 'method' => 'get')) !!}
-                123
+                {{ Form::button('送出', ['type' => 'submit', 'class' => 'btn btn-success',  'title' => '送出'] )  }}
             {!! Form::close() !!}
         </div>
     </div>
