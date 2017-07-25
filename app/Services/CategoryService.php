dbcfb3e59817c38d3f4eb4882fdff27180b4f671
jzd <jzd.tsai@gmail.com>
2017-07-25 13:31:07 +0800

    service

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -3,14 +3,13 @@ namespace App\Services;
 
 use App\User;
 use App\Category;
-use App\Repositories\CategoryRepository;
 
 class CategoryService
 {
-    public function __construct(CategoryRepository $categoryRepository)
-    {
-        $this->categoryRepository = $categoryRepository;
-    }
+//    public function __construct(CategoryRepository $categoryRepository)
+//    {
+//        $this->categoryRepository = $categoryRepository;
+//    }
     public function details($uid)
     {
         return  \DB::table('user_stores')
