fa51e25ddb99c96ba99d18253ee89279e0fc9ed5
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-08-01 19:04:01 +0800

    123

--- resources/views/backend/stores/show.blade.php
+++ resources/views/backend/stores/show.blade.php
@@ -17,7 +17,7 @@
 					<div class="container">
 						<div class="row">
 							<div class="col-md-12">
-								category:<input type="text" placeholder="limit_group" id="category">
+								category:<input type="text" placeholder="category" id="category">
 							</div>
 						</div>
 					</div>
