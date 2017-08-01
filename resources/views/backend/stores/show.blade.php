d948781cb40a5a10f3159ac408eedb35e9c0eccc
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-08-01 19:39:16 +0800

    ajax test

--- resources/views/backend/stores/show.blade.php
+++ resources/views/backend/stores/show.blade.php
@@ -35,7 +35,7 @@
 		$('#save_change').click(function () {
             $.ajax(
                 {
-					url: "backend/stores/2/category/store",
+					url: "/2/category/store",
 					method: 'POST',
                     data:'123',
 					success: function(response)
