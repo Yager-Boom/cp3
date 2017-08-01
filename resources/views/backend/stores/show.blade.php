5c5d1c06684a1c779c915e5f32c6172ef9d5af11
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-08-01 16:22:16 +0800

    asd

--- resources/views/backend/stores/show.blade.php
+++ resources/views/backend/stores/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.backend.product')
 
 @section('content')
-
+	<!-- Button trigger modal -->
 	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
 		Launch demo modal
 	</button>
