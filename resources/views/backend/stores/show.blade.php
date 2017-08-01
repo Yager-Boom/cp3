ba8a15db9c4b5a86d729e0389e3d9f85496b62cf
Yager-Booms <lovelanew30@yahoo.com.tw>
2017-08-01 19:56:18 +0800

    qwe

--- resources/views/backend/stores/show.blade.php
+++ resources/views/backend/stores/show.blade.php
@@ -35,7 +35,7 @@
 		$('#save_change').click(function () {
             $.ajax(
                 {
-                    type: 'GET',
+                    type: "GET",
 					url: "/backend/stores/2/category/store",
 					success: function(response)
 					{
