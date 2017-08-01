e27f1a3675b9013c1f3b23d2a3ec18ca2ce0d63e
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-08-01 18:52:47 +0800

    qwe

--- resources/views/backend/stores/show.blade.php
+++ resources/views/backend/stores/show.blade.php
@@ -17,9 +17,9 @@
 					<div class="container">
 						<div class="row">
 							<div class="col-md-12">
-								{!! Form::open(array('action' => array('backend\CategoryController@store',$stores->id))) !!}
-								@include('backend/categorys/_form')
-								{!! Form::close() !!}
+								{{--{!! Form::open(array('action' => array('backend\CategoryController@store',$stores->id))) !!}--}}
+								{{--@include('backend/categorys/_form')--}}
+								{{--{!! Form::close() !!}--}}
 							</div>
 						</div>
 					</div>
