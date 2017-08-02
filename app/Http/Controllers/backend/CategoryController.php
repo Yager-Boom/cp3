8019bdf086c1589142c7b31c214b082b27318375
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-08-02 15:13:30 +0800

    qw3hq

--- app/Http/Controllers/backend/CategoryController.php
+++ app/Http/Controllers/backend/CategoryController.php
@@ -36,7 +36,7 @@ class CategoryController extends Controller
 
     public function store(Request $request)
     {
-        return $_GET;
+        return $_GET['category'];
 //        try
 //        {
 //            $now = date("Y/m/d H:i ");
