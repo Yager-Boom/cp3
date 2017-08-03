64aa571d471bceb4791069817a392219a1871046
Yager-Boom <lovelanew30@yahoo.com.tw>
2017-08-03 12:41:48 +0800

    remove

--- resources/views/backend/stores/show.blade.php
+++ resources/views/backend/stores/show.blade.php
@@ -90,7 +90,7 @@
                     <div class="content">
                         <div class="fixed-table-wrap">
                             <table class="table table-sm table-bordered table-hover fixed-table"
-                                   onclick="$('#tbody').load('http://dev.nowgo.tw/backend/stores/{{$store_id}}'+' #tbody');">
+                                   onclick="$('#tbody').remove();$('#tbody').load('http://dev.nowgo.tw/backend/stores/{{$store_id}}'+' #tbody');">
                                 <thead class="thead-default">
                                 <tr>
                                     <th width="25">
